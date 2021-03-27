8855636d9a5c2863fe44d6360be04ba55749c06a
Jheyson Saavedra <jheysonsaav@gmail.com>
2021-03-26 20:22:37 -0500

    feat: update pub mod's

--- core/lib.rs
+++ core/lib.rs
@@ -1,5 +1,14 @@
+// Copyright (C) Catsh authors. All right reserved.
 pub mod colors;
+pub mod env;
 pub mod logs;
+pub mod dirs;
+
+#[derive(Debug)]
+pub struct User {
+    pub name: Option<String>,
+    pub username: String,
+}
 
 #[cfg(test)]
 mod tests {
