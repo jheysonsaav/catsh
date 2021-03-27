309e6d5812a3f1e1428e9ef6e52538f5fa751a0f
Jheyson Saavedra <jheysonsaav@gmail.com>
2021-03-27 17:05:57 -0500

    feat(core): add dirs

--- core/dirs.rs
+++ core/dirs.rs
@@ -1,39 +1,74 @@
 // Copyright (C) Catsh authors. All right reserved.
+use crate::logs::{Log, LogLevel};
 use directories::ProjectDirs;
-use std::path::{Path, PathBuf};
+use std::{fs::create_dir_all, path::PathBuf};
 
 #[derive(Debug)]
-pub enum DirLabel {
-    ConfigDir,
-    CacheDir,
-    DataDir,
-    DataLocalDir,
-    PreferenceDir,
-    RuntimeDir,
+pub struct CatshDirs {
+    pub config_dir: PathBuf,
+    pub cache_dir: PathBuf,
+    pub data_dir: PathBuf,
 }
-
-#[derive(Debug)]
-pub struct CatshDirs;
 
 impl CatshDirs {
-    pub fn config_dir() -> &'static Path {
-        Path::new("asd")
-    }
-}
-
-pub fn get_dir(label: DirLabel) -> PathBuf {
-    if let Some(dirs) = ProjectDirs::from("com", "jheysonsaav", "catsh") {
-        match label {
-            DirLabel::ConfigDir => return dirs.config_dir().to_owned(),
-            DirLabel::CacheDir => return dirs.cache_dir().to_owned(),
-            DirLabel::DataDir => return dirs.data_dir().to_owned(),
-            DirLabel::DataLocalDir => return dirs.data_local_dir().to_owned(),
-            DirLabel::PreferenceDir => return dirs.preference_dir().to_owned(),
-            DirLabel::RuntimeDir => {
-                return dirs.runtime_dir().unwrap().to_owned()
+    pub fn load() -> Self {
+        if let Some(catsh_dirs) = ProjectDirs::from("com", "catsh", "catsh") {
+            Self {
+                config_dir: catsh_dirs.config_dir().to_owned(),
+                cache_dir: catsh_dirs.cache_dir().to_owned(),
+                data_dir: catsh_dirs.data_dir().to_owned(),
+            }
+        } else {
+            Self {
+                config_dir: PathBuf::new(),
+                cache_dir: PathBuf::new(),
+                data_dir: PathBuf::new(),
             }
         }
-    } else {
-        return Path::new("catsh").to_owned();
+    }
+
+    pub fn verify(self) -> Self {
+        if self.config_dir.exists() == false {
+            match create_dir_all(self.config_dir.to_owned()) {
+                Ok(_) => {
+                    let _ = Log::new(LogLevel::Ok, 0, "Created directory");
+                }
+                Err(_) => {
+                    Log::new(LogLevel::Error, 1, "Error for create directory")
+                        .show();
+                }
+            }
+        } else {
+            Log::new(LogLevel::Ok, 0, "The directory exist");
+        }
+
+        if self.cache_dir.exists() == false {
+            match create_dir_all(self.cache_dir.to_owned()) {
+                Ok(_) => {
+                    let _ = Log::new(LogLevel::Ok, 0, "Created directory");
+                }
+                Err(_) => {
+                    Log::new(LogLevel::Error, 1, "Error for create directory")
+                        .show();
+                }
+            }
+        } else {
+            Log::new(LogLevel::Ok, 0, "The directory exist");
+        }
+
+        if self.data_dir.exists() == false {
+            match create_dir_all(self.data_dir.to_owned()) {
+                Ok(_) => {
+                    let _ = Log::new(LogLevel::Ok, 0, "Created directory");
+                }
+                Err(_) => {
+                    Log::new(LogLevel::Error, 1, "Error for create directory")
+                        .show();
+                }
+            }
+        } else {
+            Log::new(LogLevel::Ok, 0, "The directory exist");
+        }
+        self
     }
 }
--- core/lib.rs
+++ core/lib.rs
@@ -1,19 +1,11 @@
 // Copyright (C) Catsh authors. All right reserved.
 pub mod colors;
+pub mod dirs;
 pub mod env;
 pub mod logs;
-pub mod dirs;
 
 #[derive(Debug)]
 pub struct User {
     pub name: Option<String>,
     pub username: String,
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 }
