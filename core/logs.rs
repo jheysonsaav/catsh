bc43cd3cf9e7fb391d4d1a11c755ac71f2e3dcc3
Jheyson Saavedra <jheysonsaav@gmail.com>
2021-03-26 20:23:04 -0500

    feat: add documentation for logs

--- core/logs.rs
+++ core/logs.rs
@@ -1,11 +1,40 @@
+// Copyright (C) Catsh authors. All right reserved.
+
+//! ## Logs
+//! There are parts of the application where the use or registration of logs
+//! is needed either to show them to the user graphically or to register
+//! them in session files.
+//!
+//! This is where the mod logs appears, which contains functions
+//! that will help us to do this job.
+
 use crate::colors;
 
+/// these are possible levels for a log.
+/// we can use them as follows:
+/// ```rust
+/// use catsh_core::logs::LogLevel;
+///
+/// let log_level_error = LogLevel::Error;
+/// let log_level_warning = LogLevel::Warning;
+/// ```
+/// the functionality possible here is very basic,
+/// that is why it is usually used together with the `Log` structure
 #[derive(Debug)]
 pub enum LogLevel {
     Error,
     Warning,
 }
 
+/// this is the structure that a log should have, this is global for any level
+/// we can use them as follows:
+/// ```rust
+/// use catsh_core::logs::{Log, LogLevel};
+///
+/// let my_log_print: Log::new(LogLevel::Warning, 0, "This is a test log.", Some(true));
+/// let my_log__no_print: Log::new(LogLevel::Warning, 0, "This is a test log.", None);
+/// ```
+/// these are some uses that this structure can be given.
 #[derive(Debug)]
 pub struct Log {
     pub level: LogLevel,
@@ -14,6 +43,8 @@ pub struct Log {
 }
 
 impl Log {
+    /// This function allows us to create a new log in a very intuitive and easy way,
+    /// it also allows us to do extra things like print it in the console
     pub fn new(
         level: LogLevel,
         code: u16,
