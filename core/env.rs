c834a646156e890e276e324e6d870d10d2ab238d
Jheyson Saavedra <jheysonsaav@gmail.com>
2021-03-26 20:22:21 -0500

    feat: add enviroments

---
+++ core/env.rs
@@ -0,0 +1,26 @@
+use std::{env, path::PathBuf};
+
+use crate::{logs::Log, User};
+
+#[derive(Debug)]
+pub struct Env {
+    name: String,
+    value: String,
+    user: Option<User>,
+}
+
+impl Env {
+    pub fn set_var(name: &str, value: &str) -> Result<(), Log> {
+        // Set env on operative system
+        env::set_var(name, value);
+
+        // Set env on catsh
+        Self {
+            name: String::from(name),
+            value: String::from("value"),
+            user: None,
+        };
+
+        Ok(())
+    }
+}
