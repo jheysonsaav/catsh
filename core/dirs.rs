a60a7f2e7e412cbffab2a69bff6501575e80831b
Jheyson Saavedra <jheysonsaav@gmail.com>
2021-03-26 20:22:03 -0500

    feat: add dirs feature

---
+++ core/dirs.rs
@@ -0,0 +1,29 @@
+use directories::ProjectDirs;
+use std::path::{Path, PathBuf};
+
+#[derive(Debug)]
+pub enum DirLabel {
+    ConfigDir,
+    CacheDir,
+    DataDir,
+    DataLocalDir,
+    PreferenceDir,
+    RuntimeDir,
+}
+
+pub fn get(label: DirLabel) -> PathBuf {
+    if let Some(dirs) = ProjectDirs::from("com", "jheysonsaav", "catsh") {
+        match label {
+            DirLabel::ConfigDir => return dirs.config_dir().to_owned(),
+            DirLabel::CacheDir => return dirs.cache_dir().to_owned(),
+            DirLabel::DataDir => return dirs.data_dir().to_owned(),
+            DirLabel::DataLocalDir => return dirs.data_local_dir().to_owned(),
+            DirLabel::PreferenceDir => return dirs.preference_dir().to_owned(),
+            DirLabel::RuntimeDir => {
+                return dirs.runtime_dir().unwrap().to_owned()
+            }
+        }
+    } else {
+        return Path::new("catsh").to_owned();
+    }
+}
